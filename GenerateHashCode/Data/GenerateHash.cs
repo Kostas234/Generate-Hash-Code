056570c6eb963ad489eaafb8a3db0be1b1a2ca0f
Kostas234 <zakkask77@gmail.com>
2022-01-30 17:47:57 +0100

    first commit

---
+++ GenerateHashCode/Data/GenerateHash.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Security.Cryptography;
+
+namespace GenerateHashCode.Data
+{ 
+    public class GenerateHash:ComponentBase
+    {
+        public static string CreateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("X2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static string CreateSHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("X2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static string CreateSHA384(string input)
+        {
+            using (SHA384 sha384 = SHA384.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = sha384.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("X2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
